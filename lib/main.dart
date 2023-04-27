62e6b018e90922ec8c040fc923573c948792bcbb
Brian342 <migelbrian3@gmail.com>
2023-04-27 23:59:44 +0300

    windows emulator test

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,14 @@ import 'package:brianpharmacy/constraints.dart';
 import 'package:brianpharmacy/screens/admin/adminAuth/auth_page.dart';
 import 'package:brianpharmacy/screens/dashboard/home.dart';
 import 'package:brianpharmacy/screens/admin/adminPages/pharmacist.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart'; 
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
