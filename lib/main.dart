e9141558e86ca474cae6de764f6a248aee5b5193
Brian342 <migelbrian3@gmail.com>
2023-04-29 15:22:41 +0300

    changes one

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart'; 
 
-Future<void> main() async {
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+Future<void> main()  async {
+ // WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
